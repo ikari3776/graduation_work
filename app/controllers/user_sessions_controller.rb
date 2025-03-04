8d5a2e6fe8ff93ea9e5f1d85080bf1d710105b22
{ikari3776} <{nekomasa0303@icloud.com}>
2025-03-04 11:56:30 +0900

    Add: ログイン機能の追加

---
+++ app/controllers/user_sessions_controller.rb
@@ -0,0 +1,26 @@
+class UserSessionsController < ApplicationController
+  def new; end
+
+  def create
+    @user = login(params[:name], params[:password])
+    user = User.find_by(name: params[:name])
+
+    if user.nil?
+      flash.now[:danger] = "ユーザー名が存在しません"
+      render :new, status: :unprocessable_entity
+    elsif login(params[:name], params[:password])
+      flash[:success] = "ログインしました"
+      redirect_to root_path
+      return  
+    else
+      flash.now[:danger] = 'パスワードが間違っています'
+      render :new
+    end
+  end
+
+  def destroy
+    logout
+    flash[:success] = "ログアウトしました"
+    redirect_to root_path, status: :see_other
+  end
+end
