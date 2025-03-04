e39197f13f7a7b69addb1635d7afd941f93e0a12
{ikari3776} <{nekomasa0303@icloud.com}>
2025-03-04 10:29:24 +0900

    Add: ユーザー登録機能の追加

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  authenticates_with_sorcery!
+
+  validates :name, presence: true, 
+                   uniqueness: true,
+                   length: { in: 3..20 },
+                   format: { with: /\A[a-zA-Z0-9]+\z/, message: "は半角英数字のみ使用できます" }
+  validates :password, confirmation: true,
+                       length: { minimum: 3 }
+  validates :password_confirmation, length: { minimum: 3 }
+end
