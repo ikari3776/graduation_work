e3a4992f49b2860433319098f64f8ab22c37cc8c
{ikari3776} <{nekomasa0303@icloud.com}>
2025-03-08 15:44:54 +0900

    ゲーム機能の修正

--- config/routes.rb
+++ config/routes.rb
@@ -5,15 +5,15 @@ Rails.application.routes.draw do
   post "login", to: "user_sessions#create"
   delete "logout", to: "user_sessions#destroy"
   resources :rules, only: %i[index]
-  resources :games, only: [:new] do
+  resources :games, only: [ :new ] do
     collection do
-      get 'result/:game_id', to: 'games#result', as: 'result'
+      get "result/:game_id", to: "games#result", as: "result"
     end
 
-    resources :questions, only: [:show] do
+    resources :questions, only: [ :show ], param: :number do
       collection do
-        post 'answer'
-        get 'result/:question_id', to: 'questions#result', as: 'result'
+        post "answer"
+        get "result/:number", to: "questions#result", as: "result"
       end
     end
   end
