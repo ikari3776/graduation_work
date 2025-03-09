f0c7e8e3f88fcbc199e16a265f2e3e1e5f9e1b5b
{ikari3776} <{nekomasa0303@icloud.com}>
2025-03-09 23:41:45 +0900

    アプリの最終調整

---
+++ app/models/user_game_progress.rb
@@ -0,0 +1,2 @@
+class UserGameProgress < ApplicationRecord
+end
---
+++ test/models/user_game_progress_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class UserGameProgressTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
