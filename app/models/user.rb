0c2e818141bf900b646be9593a3d061ec05b29f4
{ikari3776} <{nekomasa0303@icloud.com}>
2025-03-18 16:29:29 +0900

    ユーザー編集機能、パスワードリセット機能の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,17 @@
 class User < ApplicationRecord
-  authenticates_with_sorcery!
+  authenticates_with_sorcery! do |config|
+    config.reset_password_mailer = UserMailer
+  end
 
   validates :name, presence: true,
                    uniqueness: true,
                    length: { in: 3..20 },
                    format: { with: /\A[a-zA-Z0-9]+\z/, message: "は半角英数字のみ使用できます" }
   validates :password, confirmation: true,
-                       length: { minimum: 3 }
-  validates :password_confirmation, length: { minimum: 3 }
+                       length: { minimum: 3 }, on: :create
+  validates :password_confirmation, length: { minimum: 3 }, on: :create
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }, on: :create
+  validates :reset_password_token, uniqueness: true, allow_nil: true
 
   has_many :games, dependent: :destroy
 end
