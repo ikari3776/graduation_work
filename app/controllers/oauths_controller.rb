cb1dd80132afb0720a5e2d2cd232a65b2fd2f5c6
{ikari3776} <{nekomasa0303@icloud.com}>
2025-03-20 07:14:25 +0900

    SNSログイン機能の追加

---
+++ app/controllers/oauths_controller.rb
@@ -0,0 +1,87 @@
+class OauthsController < ApplicationController
+  def oauth
+    login_at(auth_params[:provider])
+  end
+
+  def callback
+    provider = auth_params[:provider]
+    user_hash = sorcery_fetch_user_hash(provider)
+  
+    if user_hash.nil?
+      access_token = @access_token.token rescue nil
+      user_info = fetch_google_user_info(access_token)
+  
+      if user_info.present?
+        Rails.logger.info "user_info: #{user_info.inspect}" 
+        uid = user_info["sub"]
+        email = user_info["email"]
+      else
+        raise "Failed to fetch user info manually!"
+      end
+    else
+      user_info = user_hash[:user_info]
+      uid = user_hash[:uid] || user_info["sub"]
+    end
+  
+    if (@auth = Authentication.find_by(provider: provider, uid: uid))
+      @user = @auth.user
+      auto_login(@user)
+      flash[:success] = "#{provider.titleize} アカウントでログインしました"
+      redirect_to root_path
+    else
+      begin
+        signup_and_login(provider, user_info)
+        if @user.name.start_with?("firsthit")
+          session[:user_id] = @user.id
+          redirect_to new_name_path
+        else
+          flash[:success] = "#{provider.titleize} アカウントでログインしました"
+          redirect_to root_path
+        end
+      rescue StandardError => e
+        flash[:danger] = "#{provider.titleize} アカウントでのログインに失敗しました"
+        redirect_to root_path
+      end
+    end
+  end
+
+  private
+
+  def auth_params
+    params.permit(:code, :provider, :scope, :authuser, :prompt)
+  end
+
+  def signup_and_login(provider, user_info)
+    random_password = SecureRandom.hex(10)
+    temp_name = "firsthit#{SecureRandom.hex(3)}"
+  
+    @user = User.create!(
+      email: user_info["email"],
+      password: random_password,
+      password_confirmation: random_password,
+      name: temp_name
+    )
+  
+    @user.authentications.create!(
+      provider: provider,
+      uid: user_info["sub"]
+    )
+  
+    reset_session
+    auto_login(@user)
+  end
+
+  def fetch_google_user_info(access_token)
+    response = HTTParty.get("https://www.googleapis.com/oauth2/v3/userinfo", {
+      headers: {
+        "Authorization" => "Bearer #{access_token}"
+      }
+    })
+    
+    if response.code == 200
+      return JSON.parse(response.body)
+    else
+      return nil
+    end
+  end
+end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,12 +85,12 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-    address: "smtp.postmarkapp.com",
+    address: "smtp.gmail.com",
     port: 587,
-    domain: "firsthit.com",
+    domain: "gmail.com",
     authentication: "plain",
-    user_name: ENV["POSTMARK_API_TOKEN"],
-    password: ENV["POSTMARK_API_TOKEN"],
+    user_name: ENV["EMAIL_USER"],
+    password: ENV["EMAIL_PASS"],
     enable_starttls_auto: true
   }
 
